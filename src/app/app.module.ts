bc1bc841c10cd78329cc6435fa5a4f1f09165a22
Gabriel Jordão Alonso <gabrieljordaoalonso@gmail.com>
2022-05-19 19:09:33 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { MenuComponent } from './components/template/menu/menu.component';
+import { UsersComponent } from './users/users.component';
+import { ProductsComponent } from './products/products.component';
+import { ClientsComponent } from './clients/clients.component';
+import { RolesComponent } from './roles/roles.component';
+import { ScreensComponent } from './screens/screens.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    UsersComponent,
+    ProductsComponent,
+    ClientsComponent,
+    RolesComponent,
+    ScreensComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
